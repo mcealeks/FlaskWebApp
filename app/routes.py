1cabe8612a54d31c6a4c2e2204e52e535e1e080f
Piotr Kołodyński <kolek>
2018-11-24 13:22:08 +0100

    Startowy projekt

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Home Page", )
 
 @app.route('/user/<name>')
 def getUser(name):
