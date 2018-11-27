3a3e64e4636d14e77b6188f5f3a016690dfee291
Piotr Kołodyński <kolek>
2018-11-27 17:45:52 +0100

    dodano galeria.html

--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,13 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def getUser(name):
-    return render_template('user.html', name=name)
-
-@app.route('/kotki')
-def getKotki():
-    return render_template('kotki.html')
+@app.route('/galeria')
+def getGalary():
+    # Ten kod bedzie pomocniczy tak sie nie robi
+    obrazki = [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiKSmuISvOrC3rELVcZxm2YSmb3sQ8gbfJQdDs72pyu4u3UJk0',
+        'http://www.koty.pl/wp-content/uploads/2017/11/shutterstock_589722092-e1510059950350.jpg',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiKSmuISvOrC3rELVcZxm2YSmb3sQ8gbfJQdDs72pyu4u3UJk0'
+    ]
+    return render_template('galeria.html', obrazki=obrazki)
 
