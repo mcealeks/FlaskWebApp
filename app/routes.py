d0fd6d854b0b806e3403ecc910b83f21d67c679e
Piotr Kołodyński <kolek>
2018-11-28 18:31:53 +0100

    :)

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,6 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/galeria')
-def galeria():
-    #Tak sie nie robi ale jakos trza zaczac
-    obrazki = [
-        'http://www.koty.pl/wp-content/uploads/2017/11/shutterstock_589722092-e1510059950350.jpg',
-        'http://www.koty.pl/wp-content/uploads/2017/11/shutterstock_589722092-e1510059950350.jpg',
-        'http://www.koty.pl/wp-content/uploads/2017/11/shutterstock_589722092-e1510059950350.jpg',
-        'http://www.koty.pl/wp-content/uploads/2017/11/shutterstock_589722092-e1510059950350.jpg',
-    ]
-   
-    return render_template('galeria.html', obrazki=obrazki)
+
 
 
